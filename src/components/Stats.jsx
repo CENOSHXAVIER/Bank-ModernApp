07bf992c49928b1ff3a8246941e455d98009f594
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-09-12 14:59:55 +0530

    for smaller screens stats display changed from flex row to flex col and its working dopeeeeee.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,11 +2,11 @@ import styles from '../style';
 import {stats} from '../constants';
 
 const Stats = () => (
-    <section className={`ss:${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    <section className='flex justify-center items-start  flex-col sm:flex-row flex-wrap sm:mb-20 mb-6'>
       {stats.map((stat)=>
         (
           <div key={stat.id} className='flex flex-1 flex-row justify-start items-center  '>
-            <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] leading-[43px] text-white' >{stat.value}</h4>
+            <h4 className='justify-center font-poppins  font-semibold xs:text-[40px] text-[30px] leading-[43px] text-white' >{stat.value}</h4>
             <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[43px] text-gradient uppercase ml-3'>{stat.title}</p>
           </div>
         )
