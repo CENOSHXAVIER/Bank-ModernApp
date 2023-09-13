53e68e4a4d24154b40ebee7562fd68afe683d37e
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-09-13 22:28:00 +0530

    Business component completed and a button component is made inside it.Inside business component,a FeatureCard component is created and this component is put inside map().

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
+const Button = () => (
+    <button className={`bg-blue-gradient  font-poppins font-medium text-[18px] text-primary px-6 py-4 rounded-[10px] outline-none mt-4`}>
+      Get Started
+    </button>
   )
-}
+
 
 export default Button
