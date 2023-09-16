e43583283bee8b78d57c5b4be1ebb74c83657270
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-09-16 10:00:23 +0530

    feedbackCard errors fixed

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,14 +1,16 @@
 import {quotes} from '../assets'
 
 const FeedbackCard = ({name,title,img,content}) => (
-    <div className='flex flex-col rounded-[20px] max-w-[370px] mr-10 feedback-card p-3 mb-3'>
+    <div className='flex flex-col rounded-[20px] justify-between max-w-[370px] mr-10 feedback-card p-3 mb-3'>
+
       <img src={quotes} className='w-[42.6px] h-[27.6px] object-contain' />
       <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
         {content}
       </p>
-      <div className=''>
+      
+      <div className='flex flex-row justify-start items-center'>
         <img src={img} alt={name} />
-        <div className='flex flex-col'>
+        <div className='flex flex-col ml-5'>
           <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>
             {name}
           </h4>
@@ -17,6 +19,7 @@ const FeedbackCard = ({name,title,img,content}) => (
           </p>
         </div>
       </div>
+
     </div>
   )
 
