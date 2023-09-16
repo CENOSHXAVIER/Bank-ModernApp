3c562f2015fa8bf87ab1165e23f11cafd139bde5
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-09-16 12:16:01 +0530

    client section finished

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {clients} from '../constants'
+import styles from '../style'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-1 flex-row w-full`}>
+      {clients.map((client)=> (
+        <div key={client.id} className={`${styles.flexCenter} flex-1 m-5 p-5`}>
+          <img src={client.logo} alt="client" className='w-[192px]  object-contain' />
+        </div>
+      ))}
+    </div>
+    </section>
   )
-}
+
 
 export default Clients
