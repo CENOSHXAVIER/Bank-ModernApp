e209eff6789bf35879d6ad126473adaea7056991
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-09-16 11:42:35 +0530

    margin-bottom added for good mobile view

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,7 +5,7 @@ const Stats = () => (
     <section className='flex justify-center items-start  flex-col sm:flex-row flex-wrap sm:mb-20 mb-6'>
       {stats.map((stat)=>
         (
-          <div key={stat.id} className='flex flex-1 flex-row justify-start items-center  '>
+          <div key={stat.id} className='flex flex-1 flex-row justify-start items-center mb-3  '>
             <h4 className='justify-center font-poppins  font-semibold xs:text-[40px] text-[30px] leading-[43px] text-white' >{stat.value}</h4>
             <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[43px] text-gradient uppercase ml-3'>{stat.title}</p>
           </div>
